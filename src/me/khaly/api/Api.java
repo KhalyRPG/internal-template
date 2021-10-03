e4a776e65841605076a5ac10c345ff944019a865
iAtog <atogmc58vani@gmail.com>
2021-10-03 18:26:52 -0400

    release

---
+++ src/me/khaly/api/Api.java
@@ -0,0 +1,11 @@
+package me.khaly.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Api extends JavaPlugin {
+	
+	public void onEnable() {
+		getLogger().info("Api started");
+	}
+	
+}
